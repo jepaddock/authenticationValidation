8aa9ccd37b60f8adf6b7324f2b7cd03c17213209
jepaddock <jepaddock@jackhenry.com>
2020-06-08 17:33:51 -0500

    Added the 'request' require statement.

--- authenticationValidation.js
+++ authenticationValidation.js
@@ -1,5 +1,7 @@
 module.exports = exports = (req,res,next)=>{
 
+    const request = require("request");
+
     //if the client no longer has a cookie in their headers, their session is not valid, and an error will be sent. Code execution stops here.
     if(!req.headers.cookie) return res.status(401).send("User session has expired. Please log in (you may need to refresh your page) to continue working.")
 
