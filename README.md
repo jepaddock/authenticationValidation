f3a84ecba263b2c527b71563fe28d6f6059fbc60
jepaddock <jepaddock@jackhenry.com>
2020-06-08 15:20:04 -0500

    Initial commit

--- README.md
+++ README.md
@@ -10,9 +10,12 @@ const app = express();
 
 const authValidation = require("./path/of/authencticationValidation");
 
+app.use(authValidation)
 //will automatically proceed to the rest of your code if the user 
 //has been authenticated. Otherwise, the user will receive an error and code execution will stop.
-app.use(authValidation) 
 
 #the rest of your code
 ```
+
+## Dependencies
+This module is to be used within an Express application, and depends on Request.js, which you will need to add to your project's `package.json`. This might be able to be implemented using a framework other than Express, but it will likely need to be modified. 
---
+++ authenticationValidation.js
@@ -0,0 +1,21 @@
+app.use(function(req,res,next){
+
+    //if the client no longer has a cookie in their headers, their session is not valid, and an error will be sent. Code execution stops here.
+    if(!req.headers.cookie) return res.status(500).send("User session has expired. Please log in (you may need to refresh your page) to continue working.")
+
+    //otherwise, the clients's cookie SID will be pulled and checked with Authenticator to get the user's session data
+    var sessionKey = "connect.sid=s%3A" + ( req.headers.cookie.split("connect.sid=s%3A")[1] );
+    request({
+        "method" : "GET",   "url" : "http://localhost:3002/authenticator/api/userData",     "headers" : {"cookie":req.headers.cookie }
+    },
+    function(err,response,body){
+        if(!err){ //as long as there were no errors in getting the client's data, their data will be added to their session with this app, in order to be used later
+            req.session = JSON.parse(body)
+            next() //code execution will continue here.
+        }else{
+            //otherwise, send an error to the client. Code execution will stop here.
+            console.log("Error while getting user's Authenticator data:", err)
+            return res.status(500).send("There was an error getting your session data. Please refresh and try again.")
+        }
+    })
+})
