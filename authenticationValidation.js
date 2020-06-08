ed158805d7f2c7d4fa6f4e9524bf19517f1016a7
jepaddock <jepaddock@jackhenry.com>
2020-06-08 16:26:16 -0500

    The HTTP status code being returned was changed to 401, which should make error handling easier for clients.

--- authenticationValidation.js
+++ authenticationValidation.js
@@ -1,7 +1,7 @@
 module.exports = exports = (req,res,next)=>{
 
     //if the client no longer has a cookie in their headers, their session is not valid, and an error will be sent. Code execution stops here.
-    if(!req.headers.cookie) return res.status(500).send("User session has expired. Please log in (you may need to refresh your page) to continue working.")
+    if(!req.headers.cookie) return res.status(401).send("User session has expired. Please log in (you may need to refresh your page) to continue working.")
 
     //otherwise, the clients's cookie SID will be pulled and checked with Authenticator to get the user's session data
     var sessionKey = "connect.sid=s%3A" + ( req.headers.cookie.split("connect.sid=s%3A")[1] );
